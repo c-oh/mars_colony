cc546409370876fd228a2ac61d3946d980c8a38d
Holly <papiniu@outlook.com>
2016-11-15 15:12:31 -0800

    Added values to the jobs criteria

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import {Colonist, Job } from '../models';
-
+import JobsService from '../services/jobs.service'
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [JobsService]
 })
 export class RegisterComponent implements OnInit {
 
 colonist: Colonist;
-marsJobs: [Job];
+marsJobs: Job[];
 
 
-  constructor() {
-    this.colonist = new Colonist ('',null,null,null)
+  constructor(jobService: JobsService) {
+    this.colonist = new Colonist('',null,null,null);
+
+    jobService.getJobs().subscribe((jobs) => {
+      this.marsJobs = jobs;
+    }, (err)=> {
+      console.log(err);
+    });
    }
 
   ngOnInit() {
--- src/app/services/jobs.service.ts
+++ src/app/services/jobs.service.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
+
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 import {Job} from '../models';
 
 @Injectable()
-export class JobsService {
+export default class JobsService {
 
 JOBS_JSON = 'https://red-wdp-api.herokuapp.com/api/mars/jobs';
   
   constructor(private http: Http) { }
 
-  getJobs(): Observable<Response> {
-  return this.http
-    .get(this.JOBS_JSON);
-    .map((res: Response) => {
-      let body = res.json();
-      return body.data;
-    } );
-           
+  getJobs(): Observable<Job[]> {
+  return this.http.get(this.JOBS_JSON)
+    .map((res: Response) => res.json());
   }
 }
