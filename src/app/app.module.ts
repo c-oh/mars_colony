0298996c268388ccd8c6bc9ad45912448a8f3715
Holly <papiniu@outlook.com>
2016-11-15 09:34:58 -0800

    I changed the app-list line, and created four docs for my app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RegisterComponent } from './register/register.component';
+import { EncountersComponent } from './encounters/encounters.component';
+import { ReportComponent } from './report/report.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
+    RegisterComponent,
+    EncountersComponent,
+    ReportComponent,
   ],
   imports: [
     BrowserModule,
