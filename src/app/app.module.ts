e61d13772adb380f864c9e2ccbdbc255f2ce7091
Holly <papiniu@outlook.com>
2016-11-14 16:29:30 -0800

    Deleted list-test files, added a index.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ListTestComponent } from './list-test/list-test.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListTestComponent
   ],
   imports: [
     BrowserModule,
