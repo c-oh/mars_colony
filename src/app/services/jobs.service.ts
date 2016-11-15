f74f6ed3b13f9d922aba023517dff157d1cc4af0
Holly <papiniu@outlook.com>
2016-11-15 14:40:28 -0800

    Changed the weidth for css, updated jobs service file

--- src/app/services/jobs.service.ts
+++ src/app/services/jobs.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {Job} from '../models';
 
 @Injectable()
 export class JobsService {
 
 JOBS_JSON = 'https://red-wdp-api.herokuapp.com/api/mars/jobs';
-  constructor() { }
+  
+  constructor(private http: Http) { }
 
+  getJobs(): Observable<Response> {
+  return this.http
+    .get(this.JOBS_JSON);
+    .map((res: Response) => {
+      let body = res.json();
+      return body.data;
+    } );
+           
+  }
 }
