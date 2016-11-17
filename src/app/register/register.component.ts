7af8185bfccc54746404392406af8fe9f33e9a71
Holly <papiniu@outlook.com>
2016-11-17 11:34:35 -0800

    Made options show up

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -26,11 +26,7 @@ NO_JOB_SELECTED = '(none)';
     });
    }
 
-cantBe(value:string):ValidatorFn{
-  return(control:AbstractControl):{[key: string]: any} => {
-    return control.value === value ? {'cant be none' :{value}} :null;
-  };
-}
+
   ngOnInit() {
 
     this.registerForm = new FormGroup({
