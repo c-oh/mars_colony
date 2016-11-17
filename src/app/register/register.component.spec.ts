0848b70bb66bef307c2815511d21af04dc84d684
Holly <papiniu@outlook.com>
2016-11-17 11:13:55 -0800

    Trying to make the form accept and record new data

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -2,7 +2,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { FormGroup, FormControl, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import AliensService from '../services/aliens.service';
 import EncountersService from '../services/encounters.service';
-
+import { FormGroup, FormControl, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 import{Alien} from '../models';
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.css']
+  styleUrls: ['./report.component.css'],
+  providers: [AliensService, EncountersService]
 })
 
 export class ReportComponent implements OnInit {
 
 aliensList:Alien[];
+NO_ALIEN_SELECTED = '(none)';
+reportForm: FormGroup;
 
   constructor(private aliensService: AliensService,
   private encountersService: EncountersService) { 
@@ -24,6 +27,10 @@ aliensList:Alien[];
   }
 
   ngOnInit() {
+    this.reportForm= new FormGroup({
+      atype: new FormControl(this.NO_ALIEN_SELECTED, []),
+      action: new FormControl('',[Validators.required,Validators.maxLength(450)])
+    })
   }
 
 }
